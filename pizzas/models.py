08155fb4558154e7f3d9fce69a4ebdea7282bfa4
Huimin <huimin_mu1@baylor.edu>
2021-11-21 15:47:53 -0600

    Create models Pizza and Topping

--- pizzas/models.py
+++ pizzas/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=200)
+
+class Topping(models.Model):
+    pizza = models.ForeignKey(Pizza, on_delete=CASCADE)
+    name = models.CharField(max_length=200)
+    

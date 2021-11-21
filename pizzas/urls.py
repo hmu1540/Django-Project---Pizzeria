b485703fe7643035b89b40316535f0a23f2c379b
Huimin <huimin_mu1@baylor.edu>
2021-11-21 17:28:32 -0600

    Create a urls.py file for pizzas

---
+++ pizzas/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+import pizzas
+
+app_name = pizzas 
+# The variable app_name helps Django distinguish this urls.py file from files of the same name in other apps within the project

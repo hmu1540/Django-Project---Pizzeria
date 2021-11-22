9ab06da93dd83e7d654fbd1a33f2dd0782fa4e6d
Huimin <huimin_mu1@baylor.edu>
2021-11-21 18:37:37 -0600

    Write view functions of pizzas and pizza

--- pizzas/views.py
+++ pizzas/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from .models import Pizza, Topping
 
 # Create your views here.
 
 def index(request):
     return render(request, 'pizzas/index.html')
+
+def pizzas(request):
+
+    return render(request, 'pizzas/pizzas.html')
+
+def pizza(request, pizza_id):
+    pizza = Pizza.objects.get(id = pizza_id)
+    toppings = pizza.topping_set.all()
+    context = {'pizza': pizza, 'toppings': toppings}
+
+    return render(request, 'pizzas/pizza.html', context)

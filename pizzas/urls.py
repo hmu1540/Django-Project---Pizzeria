3ede15a1a0b882cb4736cb45a62174c71c9bb027
Huimin <huimin_mu1@baylor.edu>
2021-11-21 18:36:34 -0600

    Add two urls to match pizzas page and pizza page seperately

--- pizzas/urls.py
+++ pizzas/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from . import views
 import pizzas
 
-app_name = pizzas 
+app_name = 'pizzas' 
 # The variable app_name helps Django distinguish this urls.py file from files of the same name in other apps within the project
 
 
 urlpatterns = [
     path('', views.index, name='index'),
     # Django ignores the base URL for the project (http://localhost:8000/), so the empty string ('') matches the base URL. 
+    path('pizzas', views.pizzas, name='pizzas'),
+    path('pizzas/<int: pizza_id>/', views.pizza, name='pizza'),
 ]

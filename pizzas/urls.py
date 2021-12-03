5ccb1b4e6ec1e59893683d3459fbf88e6b9b37da
Huimin <huimin_mu1@baylor.edu>
2021-12-02 23:25:01 -0600

    Add comment url

--- pizzas/urls.py
+++ pizzas/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     # Django ignores the base URL for the project (http://localhost:8000/), so the empty string ('') matches the base URL. 
     path('pizzas', views.pizzas, name='pizzas'),
-    path('pizzas/<int: pizza_id>/', views.pizza, name='pizza'),
+    path('pizzas/<int:pizza_id>/', views.pizza, name='pizza'),
+    path('comment/<int:pizza_id>/', views.comment, name ='comment'),
 ]

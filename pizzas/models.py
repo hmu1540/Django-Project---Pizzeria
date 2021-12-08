c0c1d64df3bac44036293078ccfc8dbfa92eb01d
Huimin <huimin_mu1@baylor.edu>
2021-12-08 14:17:08 -0600

    Add comment model

--- pizzas/models.py
+++ pizzas/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
+from django.db.models.fields.json import CaseInsensitiveMixin
 
 # Create your models here.
 
@@ -16,3 +17,9 @@ class Topping(models.Model):
     def __str__(self):
         return self.name
     
+class Comment(models.Model):
+    pizza = models.ForeignKey(Pizza, on_delete=CASCADE)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
